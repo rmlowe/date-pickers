bdbf9307e1a579188a77c75f40dc391ba16be636
Robert Lowe <robertmlowe@rmlowe.com>
2020-03-14 15:59:38 +0800

    Using react-date-range

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
+// import 'react-dates/initialize';
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import { DateRangePicker } from 'react-dates';
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRange, DateRangePicker } from 'react-date-range';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    dateRange: {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: 'selection',
+    }
+  }
+
+  handleSelect = ranges => {
+    console.log(ranges);
+    this.setState({ dateRange: ranges.selection });
+  }
+
+  render() {
+    const selectionRange = {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: 'selection',
+    }
+    return (
+      <div className="App">
+        {/* <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header> */}
+        {/* <DateRangePicker
+          startDate={this.state.startDate} // momentPropTypes.momentObj or null,
+          startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
+          endDate={this.state.endDate} // momentPropTypes.momentObj or null,
+          endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
+          onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
+          focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+          onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
+        /> */}
+        <DateRange
+          ranges={[this.state.dateRange]}
+          onChange={this.handleSelect}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
